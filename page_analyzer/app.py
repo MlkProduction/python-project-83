4aeba90fe0f09cd9be879d440947d88cfa009751
Георгий Малков <georgijmalkov@MacBook-Air-Georgij.local>
2025-03-25 14:46:54 +0400

    two task

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,10 +1,20 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 from dotenv import load_dotenv
 
 load_dotenv()
 
 app = Flask(__name__)
 
-# Устанавливаем SECRET_KEY
+
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
+@app.route('/')
+def courses_index():
+
+    return render_template(
+        'index.html',
+    )
+
+if __name__ == "__main__":
+    app.run(debug=True)
