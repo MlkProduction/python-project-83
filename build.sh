475e0bc6be0be499f30875d0b5d7f327309b6c9b
Георгий Малков <georgijmalkov@MacBook-Air-Georgij.local>
2025-06-05 16:23:51 +0400

    change build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 # скачиваем uv и запускаем команду установки зависимостей
 curl -LsSf https://astral.sh/uv/install.sh | sh
-source $HOME/.local/bin/env
+
+export PATH="$HOME/.local/bin:$PATH"
+
+uv sync
+
 make install && psql -a -d $DATABASE_URL -f database.sql
