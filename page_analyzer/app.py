ffbac0a455756de78e678e8bc606b453c07b41ff
Георгий Малков <georgijmalkov@MacBook-Air-Georgij.local>
2025-06-14 11:48:57 +0400

    исправлены ошибки html

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -26,7 +26,7 @@ load_dotenv()
 
 app = Flask(__name__)
 
-app.config["SECRET_KEY"] = os.getenv("SECRET_KEY") #NOSONAR 
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY") # NOSONAR 
 app.config["DATABASE_URL"] = os.getenv("DATABASE_URL")
 
 repo = UrlsRepository(app.config["DATABASE_URL"])
